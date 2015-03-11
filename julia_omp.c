7bb5c47e91bdb674487b4a8c99b5810997604c6c
Brandon Byskov <brandonbyskov@gmail.com>
2015-03-10 20:01:14 -0400

    Mostly working julia_omp.c

--- julia_omp.c
+++ julia_omp.c
@@ -7,29 +7,35 @@
 int julia(const double *x, int xres, const double *y, int yres, const double *c, 
 		int flag, int maxIterations, int *iterations)
 {
-	int maxIterationCount = 0, i,j;
+	int maxIterationCount = 0;
 
-	//omp_set_num_threads(4);
-	#pragma omp parallel //private(nthreads, tid)
+	omp_set_num_threads(4);
+	#pragma omp parallel reduction(max:maxIterationCount)
 	{
 		int nthreads, tid;
 		tid = omp_get_thread_num();
 		nthreads = omp_get_num_threads();
 		printf("thread: %d, Max: %d\n", tid, nthreads);
-	}
+	
 
 	double a, b, xgap, ygap, cr, ci, radius;
 	int count;
-	xgap = (x[1] - x[0]) / xres;
-	ygap = (y[1] - y[0]) / yres;
 
-	for (j = 0; j < yres; j++)
+	//thread private variables
+	int *p_iterations = iterations + ((yres/nthreads)* xres * tid);
+	int t_yres = yres/nthreads + ((tid == nthreads - 1)? yres % nthreads : 0);
+	double t_y0 = y[0] + (y[1] - y[0])*tid/nthreads;
+	xgap = (x[1] - x[0]) / xres;
+	ygap = ((y[1] - y[0]) / t_yres) / nthreads;
+
+	int i,j;
+	for (j = 0; j < t_yres; j++)
 	{
 		for (i = 0; i < xres; i++)    
 		{
 			/* pixel to coordinates */
 			a = x[0] + i * xgap;
-			b = y[0] + j * ygap;
+			b = t_y0 + j * ygap;
 
 			/* initial value for the iteration */
 			cr = flag*c[0] + (1-flag)*a;
@@ -51,7 +57,7 @@ int julia(const double *x, int xres, const double *y, int yres, const double *c,
 			if(count > maxIterationCount )
 				maxIterationCount = count;
 
-			int *p = iterations + j*xres+i;
+			int *p = p_iterations + j*xres+i;
 
 			/* If radius <= 4.0, we have hit maxIterations. The point is
 			likely in the set. */
@@ -68,6 +74,8 @@ int julia(const double *x, int xres, const double *y, int yres, const double *c,
 			}
 		}
 	}
+
+}
 
 	return maxIterationCount;
 }
